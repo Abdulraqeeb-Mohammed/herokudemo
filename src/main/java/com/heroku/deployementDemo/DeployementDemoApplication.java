e4b55778f0bafae1585f434f82d7ce3fbb6b20b0
Abdulraqeeb-Mohammed <abdo.itis@gmail.com>
2021-05-17 22:05:32 +0300

    first commit

---
+++ src/main/java/com/heroku/deployementDemo/DeployementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.deployementDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployementDemoApplication.class, args);
+	}
+
+}
